d3bf4c6a3b55f74ff96a199a2ac1fc2f183831ff
dhcohn <davidhcohn@gmail.com>
2018-12-10 14:52:59 -0500

    step 2

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -56,8 +56,16 @@ const shoppingList = (function(){
   
   
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
-  }
+    try { 
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+      render();
+
+    }
+    catch(error){
+      console.log('Cannot add item: {error.message}');
+    }
+  };
   
   function handleNewItemSubmit() {
     $('#js-shopping-list-form').submit(function (event) {
